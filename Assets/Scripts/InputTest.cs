2c0e0632e3383617d98a010ce566d17f397bd18c
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2023-06-13 14:25:56 +0900

    임시

---
+++ Assets/Scripts/InputTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputTest : MonoBehaviour
+{
+    public Text text;
+    public Canvas canvas;
+    public GameObject note;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string s = "";
+        for(int i = 0; i < Input.touchCount; i++)
+        {
+            s += Input.GetTouch(i).position;
+        }
+        text.text = s;
+    }
+}
