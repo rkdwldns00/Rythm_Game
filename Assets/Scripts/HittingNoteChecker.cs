72ddc3f5ea76485cfc6939536745537cc47cbd66
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2023-06-14 17:32:27 +0900

    플릭 입력 구현

--- Assets/Scripts/HittingNoteChecker.cs
+++ Assets/Scripts/HittingNoteChecker.cs
@@ -16,18 +16,24 @@ public class HittingNoteChecker : MonoBehaviour
     public Canvas canvas;
     public GameObject touchCheck;
     public TouchMode[] TouchDatas { get; private set; } = new TouchMode[12];
-    public Vector2[] FlickDatas { get; private set; }= new Vector2[12];
+    Queue<Vector2>[] flickDatas = new Queue<Vector2>[12];
     bool[] holdingDatas = new bool[12];
 
     void Start()
     {
-
+        for(int i=0;i<flickDatas.Length;i++)
+        {
+            flickDatas[i] = new Queue<Vector2>();
+            flickDatas[i].Enqueue(Vector2.zero);
+            flickDatas[i].Enqueue(Vector2.zero);
+            flickDatas[i].Enqueue(Vector2.zero);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Log.text = GetFlickPower(0).ToString();
     }
 
     private void LateUpdate()
@@ -40,6 +46,15 @@ public class HittingNoteChecker : MonoBehaviour
             }
         }
         holdingDatas = new bool[12];
+
+        for (int i = 0; i < flickDatas.Length; i++)
+        {
+            flickDatas[i].Dequeue();
+            if (flickDatas[i].Count < 3)
+            {
+                flickDatas[i].Enqueue(Vector2.zero);
+            }
+        }
     }
 
     public void HitLine(int lineIndex, TouchMode touchMode, Vector2 moveSpeed)
@@ -55,7 +70,18 @@ public class HittingNoteChecker : MonoBehaviour
             holdingDatas[lineIndex] = true;
         }
 
-        FlickDatas[lineIndex] = moveSpeed;
+        flickDatas[lineIndex].Enqueue(moveSpeed);
         TouchDatas[lineIndex] = touchMode;
+    }
+
+    public Vector2 GetFlickPower(int lineIndex)
+    {
+        Vector2 sum = Vector2.zero;
+        foreach(Vector2 v in flickDatas[lineIndex])
+        {
+            sum += v;
+        }
+        sum.y *= -1;
+        return sum;
     }
 }
