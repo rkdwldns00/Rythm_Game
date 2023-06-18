a130631e1fe435be46e9c67756cf635459df75de
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2023-06-19 00:50:03 +0900

    입력표시 개선

--- Assets/Scripts/TouchLineBox.cs
+++ Assets/Scripts/TouchLineBox.cs
@@ -6,20 +6,21 @@ using UnityEngine.UI;
 public class TouchLineBox : MonoBehaviour
 {
     Image render;
-    public float showTime = 0.2f;
-    public float originAlpha = 0.5f;
+    public float showTime;
+    public float originAlpha;
     float timer = 0.01f;
 
     void Start()
     {
         render = GetComponent<Image>();
+        render.color = new Color(1, 1, 1, originAlpha);
     }
 
     // Update is called once per frame
     void Update()
     {
+        render.color = new Color(1, 1, 1, (showTime - timer) / showTime * originAlpha);
         timer += Time.deltaTime;
         if(timer > showTime) { Destroy(gameObject); }
-        render.color = new Color(1, 1, 1, (showTime - timer) / showTime);
     }
 }
