8c18ba952b336a8cde5d460812d3e29cba8fec17
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2023-08-09 19:25:32 +0900

    맵 에디터 스크롤 구현

--- Assets/Scripts/Map Editor/MapEditManager.cs
+++ Assets/Scripts/Map Editor/MapEditManager.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class MapEditManager : MonoBehaviour
 {
     public static SavedMapData EditingMap { get; private set; }
 
     public static MapEditManager Instance { get; private set; }
+
+    [Header("스크롤 관리용 참조")]
+    [SerializeField] Transform mapScrollViewContent;
+    [SerializeField] Transform mapScrollViewFirstLine;
 
     public static void StartMapEditScene(SavedMapData mapData)
     {
@@ -22,5 +28,13 @@ public class MapEditManager : MonoBehaviour
             Debug.LogError("편집할 맵이 존재하지 않습니다!");
         }
         Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    public void OnScrollMapScrollView()
+    {
+        if(mapScrollViewContent.transform.localPosition.y <= 0)
+        {
+            mapScrollViewContent.transform.localPosition = Vector3.zero;
+        }
     }
 }
