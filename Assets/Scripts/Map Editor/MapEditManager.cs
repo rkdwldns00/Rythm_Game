0f8331d1b09fc3549fb8f3e526c52db61977270f
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2023-08-11 11:10:41 +0900

    맵에디터에 박 표시용 라인 소환

--- Assets/Scripts/Map Editor/MapEditManager.cs
+++ Assets/Scripts/Map Editor/MapEditManager.cs
@@ -12,7 +12,12 @@ public class MapEditManager : MonoBehaviour
 
     [Header("스크롤 관리용 참조")]
     [SerializeField] Transform mapScrollViewContent;
-    [SerializeField] Transform mapScrollViewFirstLine;
+    [SerializeField] Transform mapScrollInLine;
+    [Header("마디선 프리펩")]
+    [SerializeField] GameObject barLinePrefab;
+    [Header("기타 설정")]
+    public float spacing;
+    public float firstBarLineYPos = -1800;
 
     public static void StartMapEditScene(SavedMapData mapData)
     {
@@ -28,11 +33,17 @@ public class MapEditManager : MonoBehaviour
             Debug.LogError("편집할 맵이 존재하지 않습니다!");
         }
         Screen.orientation = ScreenOrientation.Portrait;
+
+        for(int i = 0; i < 100; i++)
+        {
+            GameObject bar = Instantiate(barLinePrefab, mapScrollInLine);
+            bar.transform.localPosition = new Vector3(bar.transform.localPosition.x, firstBarLineYPos + i * spacing, 0);
+        }
     }
 
     public void OnScrollMapScrollView()
     {
-        if(mapScrollViewContent.transform.localPosition.y <= 0)
+        if(mapScrollViewContent.transform.localPosition.y >= 0)
         {
             mapScrollViewContent.transform.localPosition = Vector3.zero;
         }
