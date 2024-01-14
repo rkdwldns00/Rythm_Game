a7494a01937fce95343d4d9f07c8f314ef460019
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2024-01-14 22:26:05 +0900

    맵 에디터 맵 정보 UI 로직 구현

--- Assets/Scripts/Map Editor/MapInfoSettingUI.cs
+++ Assets/Scripts/Map Editor/MapInfoSettingUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MapInfoSettingUI : MonoBehaviour
@@ -17,12 +18,69 @@ public class MapInfoSettingUI : MonoBehaviour
     [SerializeField] private Text bgmOffsetText;
     [SerializeField] private Slider startBpmSlider;
     [SerializeField] private Text startBpmText;
-    [Header("")]
-    [SerializeField] private InputField meterInput1;
-    [SerializeField] private InputField meterInput2;
     [Header("메뉴 전환 버튼")]
+    [SerializeField] private Button openInfoSettingMenuButton;
     [SerializeField] private Button toMenuButton;
     [SerializeField] private Button backToEditButton;
 
-    private MapEditManager mapEditManager => MapEditManager.Instance;
+    private void Awake()
+    {
+        saveButton.onClick.AddListener(SaveMap);
+        loadButton.onClick.AddListener(LoadMap);
+        mapNameInput.onValueChanged.AddListener(SetMapName);
+        mapArtistNameInput.onValueChanged.AddListener(SetArtistName);
+        mapDesignerNameInput.onValueChanged.AddListener(SetDesignerName);
+        bgmOffsetSlider.onValueChanged.AddListener(SetBgmOffset);
+        startBpmSlider.onValueChanged.AddListener(SetBgmOffset);
+        openInfoSettingMenuButton.onClick.AddListener(() => SetUIEnable(true));
+        toMenuButton.onClick.AddListener(GotoMenu);
+        backToEditButton.onClick.AddListener(() => SetUIEnable(false));
+    }
+
+    public void SetUIEnable(bool enable)
+    {
+        gameObject.SetActive(enable);
+    }
+
+    private void SaveMap()
+    {
+        MapFileUtil.SaveMapResource(MapEditManager.EditingMap);
+    }
+
+    private void LoadMap()
+    {
+        //임시
+    }
+
+    private void SetMapName(string name)
+    {
+        MapEditManager.EditingMap.title = name;
+    }
+
+    private void SetArtistName(string name)
+    {
+        MapEditManager.EditingMap.artistName = name;
+    }
+
+    private void SetDesignerName(string name)
+    {
+        MapEditManager.EditingMap.designerName = name;
+    }
+
+    private void SetBgmOffset(float offset)
+    {
+        MapEditManager.EditingMap.startOffset = offset;
+        bgmOffsetText.text = offset.ToString();
+    }
+
+    private void SetBPM(float bpm)
+    {
+        MapEditManager.EditingMap.startBpm = bpm;
+        startBpmText.text = bpm.ToString();
+    }
+
+    private void GotoMenu()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
 }
