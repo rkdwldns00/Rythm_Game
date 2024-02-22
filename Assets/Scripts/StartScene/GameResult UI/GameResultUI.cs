f57aaa9d7547f9b4a19afe90511baab805630b1e
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2024-02-23 03:43:28 +0900

    게임 결과 UI 디자인

---
+++ Assets/Scripts/StartScene/GameResult UI/GameResultUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameResultUI : MonoBehaviour
+{
+    [SerializeField] Text scoreText;
+    [SerializeField] Text comboText;
+    [SerializeField] Text perfectText;
+    [SerializeField] Text greatText;
+    [SerializeField] Text goodText;
+    [SerializeField] Text badText;
+    [SerializeField] Text missText;
+}
